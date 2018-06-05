59c255bd390420f0d91c9f1182765868fe566150
Xin Wang <xin@nervanasys.com>
2018-06-05 11:43:27 -0700

    WIP: ART data loader

---
+++ data.py
@@ -0,0 +1,111 @@
+import os
+import pickle
+import random
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+NUM_EXAMPLES = {'train': 100000, 'val': 10000, 'test': 20000}  # sizes of datasets
+KEYS = map(chr, range(97, 97 + 26))  # 26 letters
+VALS = map(str, range(10))  # 10 digits
+PROMPT = '?'
+SEED = .666  # random seed for permutation in task3
+
+
+def ensure_dir_exist(path):
+    outdir = os.path.dirname(path)
+    if outdir != '' and not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    return path
+
+
+def code_book(keys, vals, prompt):
+    return dict(zip(keys + vals + [prompt], range(len(keys) + len(vals) + 1)))
+
+
+def generate_art_example(num_pairs, keys, vals, prompt, task, shuffle_fn):
+    nkeys, nvals = len(keys), len(vals)
+    current_keys, current_vals, current_pairs = [], [], {}
+    # sample from task example space
+    for i in range(0, num_pairs):
+        key = keys[random.randint(0, nkeys - 1)]
+        while key in current_pairs.keys():
+            key = keys[random.randint(0, nkeys - 1)]
+        val = vals[random.randint(0, nvals - 1)]
+        current_pairs[key] = val
+        current_keys += key
+        current_vals += val
+    query = random.choice(list(current_pairs.keys()))
+    target = current_pairs[query]
+    # synthesize the sequence
+    if task == 1:
+        x = [item for pair in zip(current_keys, current_vals) for item in pair]
+    elif task == 2:
+        x = current_keys + current_vals
+    elif task == 3:
+        shuffle_fn(current_vals)
+        x = current_keys + current_vals
+    x += [prompt, prompt, query]
+    return x, target
+
+
+def encode_art_example(x, y, word_idx):
+    x_enc = np.array([word_idx[w] for w in x])
+    y_onehot = np.zeros([len(word_idx)])
+    y_onehot[word_idx[y]] = 1
+    return x_enc, y_onehot
+
+
+def generate_art_data(num_examples, num_pairs, keys, vals, prompt, task, shuffle_fn):
+    word_idx = code_book(keys, vals, prompt)
+    x = np.zeros([num_examples, num_pairs * 2 + 3], dtype=np.float32)
+    y = np.zeros([num_examples, len(word_idx)], dtype=np.float32)
+    for i in range(num_examples):
+        _x, _y = generate_art_example(num_pairs, keys, vals, prompt, task, shuffle_fn)
+        x[i], y[i] = encode_art_example(_x, _y, word_idx)
+    return {'x': x, 'y': y, 'word_idx': word_idx, 'n': num_examples}
+
+
+def load_art_data(data_dir, task, num_pairs, dset):
+    data_file = "{}/associative-retrieval-task{}_{}pairs_{}.pkl".format(
+        ensure_dir_exist(data_dir), task, num_pairs, dset)
+    if os.path.exists(data_file):
+        print("Loading ART data from {}".format(data_file))
+        with open(data_file, 'rb') as f:
+            data = pickle.load(f)
+    else:
+        print("Generating ART data: {} examples of task {} of {} pairs".format(NUM_EXAMPLES[dset], task, num_pairs))
+        data = generate_art_data(
+            NUM_EXAMPLES[dset],
+            num_pairs,
+            keys=KEYS,
+            vals=VALS,
+            prompt=PROMPT,
+            task=task,
+            shuffle_fn=lambda ls: random.shuffle(ls, lambda: SEED))
+        print("Dumping ART data to {}".format(data_file))
+        with open(data_file, 'wb') as f:
+            pickle.dump(data, f, protocol=2)
+    return data
+
+
+class ARTDataset(Dataset):
+
+    def __init__(self, data_dir='./data', task=1, num_pairs=8, dset='train'):
+        self.data = load_art_data(data_dir, task, num_pairs, dset)
+
+    def __len__(self):
+        return self.data['n']
+
+    def __getitem__(self, idx):
+        assert idx < self.__len__(), "index {} out of bound ({})".format(idx, self.__len__())
+        return (self.data['x'][idx], self.data['y'][idx])
+
+
+class ART(object):
+
+    def __init__(self, data_dir='./data', task=1, num_pairs=8, batch_size=64, shuffle=True, cuda=False):
+        kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+        self.train = DataLoader(
+            ARTDataset(data_dir, task, num_pairs, dset='train'), batch_size=batch_size, shuffle=shuffle, **kwargs)
+        self.val = DataLoader(ARTDataset(data_dir, task, num_pairs, dset='val'), batch_size=batch_size, **kwargs)
+        self.test = DataLoader(ARTDataset(data_dir, task, num_pairs, dset='test'), batch_size=batch_size, **kwargs)
--- train_art.py
+++ train_art.py
@@ -3,6 +3,7 @@ import os
 import random
 import numpy as np
 import argparse
+from data import ART
 
 parser = argparse.ArgumentParser(description='Learn associative retrieval tasks')
 parser.add_argument('--data-dir', default='./data', help='data directory (default: ./data)')
@@ -17,39 +18,47 @@ parser.add_argument('--seed', type=int, default=0, help='random seed (default: 0
 args = parser.parse_args()
 
 # ART data
-data_file = 'associative-retrieval_task{}_{}pairs.pkl'.format(args.task, args.num_pairs)
-with open(os.path.join(args.data_dir, data_file), 'rb') as f:
-    data = pickle.load(f)
+dataset = ART(
+    data_dir=args.data_dir,
+    task=args.task,
+    num_pairs=args.num_pairs,
+    batch_size=args.batch_size,
+    shuffle=True,
+    cuda=True)
 
-word_idx = data['word_idx']
-vocab_size = len(word_idx)
-x_len = data['x_train'].shape[1]
-n_train = data['x_train'].shape[0]
-n_test = data['x_test'].shape[0]
-n_val = data['x_val'].shape[0]
-train_labels = np.argmax(data['y_train'], axis=1)
-test_labels = np.argmax(data['y_test'], axis=1)
-val_labels = np.argmax(data['y_val'], axis=1)
+# data_file = 'associative-retrieval_task{}_{}pairs.pkl'.format(args.task, args.num_pairs)
+# with open(os.path.join(args.data_dir, data_file), 'rb') as f:
+#     data = pickle.load(f)
 
-print("x_train", data['x_train'][0])
-print("y_train", data['y_train'][0])
-print("Training set shape", data['x_train'].shape)
-print("Training Size", n_train)
-print("Validation Size", n_val)
-print("Testing Size", n_test)
+# word_idx = data['word_idx']
+# vocab_size = len(word_idx)
+# x_len = data['x_train'].shape[1]
+# n_train = data['x_train'].shape[0]
+# n_test = data['x_test'].shape[0]
+# n_val = data['x_val'].shape[0]
+# train_labels = np.argmax(data['y_train'], axis=1)
+# test_labels = np.argmax(data['y_test'], axis=1)
+# val_labels = np.argmax(data['y_val'], axis=1)
 
-batches_train = zip(range(0, n_train - args.batch_size, args.batch_size), range(args.batch_size, n_train, args.batch_size))
-batches_train = [(start, end) for start, end in batches_train]
-batches_val = zip(range(0, n_val - args.batch_size, args.batch_size), range(args.batch_size, n_val, args.batch_size))
-batches_val = [(start, end) for start, end in batches_val]
-batches_test = zip(range(0, n_test - args.batch_size, args.batch_size), range(args.batch_size, n_test, args.batch_size))
-batches_test = [(start, end) for start, end in batches_test]
+# print("x_train", data['x_train'][0])
+# print("y_train", data['y_train'][0])
+# print("Training set shape", data['x_train'].shape)
+# print("Training Size", n_train)
+# print("Validation Size", n_val)
+# print("Testing Size", n_test)
 
-batches = batches_train
-for t in range(1, args.epochs + 1):
-    random.Random(args.seed+t).shuffle(batches)
-    for b_idx_train, (start, end) in enumerate(batches):
-        x_train = data['x_train'][start:end]
-        x_train_lens = [x_len] * args.batch_size
-        y_train = data['y_train'][start:end]
-        import ipdb; ipdb.set_trace()
+# batches_train = zip(range(0, n_train - args.batch_size, args.batch_size), range(args.batch_size, n_train, args.batch_size))
+# batches_train = [(start, end) for start, end in batches_train]
+# batches_val = zip(range(0, n_val - args.batch_size, args.batch_size), range(args.batch_size, n_val, args.batch_size))
+# batches_val = [(start, end) for start, end in batches_val]
+# batches_test = zip(range(0, n_test - args.batch_size, args.batch_size), range(args.batch_size, n_test, args.batch_size))
+# batches_test = [(start, end) for start, end in batches_test]
+
+# batches = batches_train
+# for t in range(1, args.epochs + 1):
+#     random.Random(args.seed+t).shuffle(batches)
+#     for b_idx_train, (start, end) in enumerate(batches):
+#         x_train = data['x_train'][start:end]
+#         x_train_lens = [x_len] * args.batch_size
+#         y_train = data['y_train'][start:end]
+#         import ipdb; ipdb.set_trace()
